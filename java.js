ecfa21cc014d0a68d77d422bfbc812cce2357de3
alee016 <133073742+alee016@users.noreply.github.com>
2023-12-05 16:03:59 -0500

    Update java.js

--- java.js
+++ java.js
@@ -3,27 +3,30 @@ document.getElementById('fetchButton').addEventListener('click', fetchData);
 function fetchData() {
     fetch('main.json')
         .then(response => {
-			alert("Response");
+            alert("Response");
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.json();
         })
         .then(data => {
-			alert("Data");
+            alert("Data");
             console.log('Data:', data);
             processJSONData(data);
         })
         .catch(error => {
-			alert("Error " + error);
+            alert("Error " + error);
             console.error('Error:', error);
         });
-		
+
 }
 
 function processJSONData(data) {
     const dataDiv = document.getElementById("data");
     dataDiv.innerhtml = "<h2>College Degrees:</h2><pre>" + JSON.stringify(data, null, 2) + "</pre>";
 
+
+
+}
 
 }
