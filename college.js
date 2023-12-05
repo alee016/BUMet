881439aeb8053028ada74fb8f9f641dcc5259664
alee016 <133073742+alee016@users.noreply.github.com>
2023-12-04 20:16:16 -0500

    Update college.js

--- college.js
+++ college.js
@@ -1,31 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const fetchDataButton = document.getElementById("fetchDataButton");
-  
-    fetchDataButton.addEventListener("click", fetchData);
-  
-    function fetchData() {
-      const jsonFileUrl = "https://alee016.github.io/BUMet/";
-  
-      fetch(jsonFileUrl)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-  
-          return response.json();
-        })
-        .then((data) => {
-          console.log("Fetched Data:", data);
-  
-          data.degrees.forEach((degree) => {
-            console.log(`Degree from ${degree.school}: ${degree.type} in ${degree.program}`);
-          });
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  // Attach a click event listener to a button with id 'fetchButton'
+  document.getElementById('fetchButton').addEventListener('click', function () {
+    // Replace 'YOUR_JSON_URL' with the actual URL where your JSON file is hosted
+    const jsonUrl = 'https://alee016.github.io/BUMet/';
+
+    // Create a fetch request to the JSON file
+    fetch(jsonUrl)
+      .then(response => {
+        // Check if the fetch request was successful (status code 200-299)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Parse the JSON response
+        return response.json();
+      })
+      .then(data => {
+        // Process the returned JSON data
+        console.log('Degrees Data:', data);
+
+        // Replace this part with your specific processing logic
+        // For example, you can display the degrees information on the webpage
+        displayDegrees(data);
+      })
+      .catch(error => {
+        console.error('Fetch Error:', error);
+      });
   });
 
+  // Function to display degrees information (replace this with your UI logic)
+  function displayDegrees(degrees) {
+    // Example: Displaying degrees in the console
+    degrees.forEach(degree => {
+      console.log(`School: ${degree.school}, Program: ${degree.program}, Type: ${degree.type}, Year Conferred: ${degree.yearConferred}`);
+    });
+  }
+});
 
 
